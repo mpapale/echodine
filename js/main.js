e43941e37ca8d3f2c6daa700b2db634ec90fd8b1
Mike Papale <mpapale@gmail.com>
2013-02-17 15:46:47 -0800

    add backbone and require, plus a simple view of the data

--- js/main.js
+++ js/main.js
@@ -1,30 +1,109 @@
-(function() {
-	var global = window;
+require.config({
+	shim: {
+		"contrib/underscore": {
+			exports: "_"
+		},
+		"contrib/backbone": {
+			deps: ["contrib/underscore", "jquery"],
+			exports: "Backbone"
+		},
+		"contrib/bootstrap": {
+			deps: ["jquery"],
+			exports: "$.fn.popover" // kind of a hack, we choose one of bootstrap's "exports" arbitrarily
+		}
+	},
 
-	// dependencies. we'll replace this with require js if i feel like it
-	var _ = window._; // underscore
-	var $ = window.$; // jquery
-	var Backbone = window.Backbone; 
-
-	// setup some models
-	var ProductModel = Backbone.Model.extend({
-		imageUrl: '',
-		sizes: [], // list of floats, in fluid ounces
-		price: 0.0, // in dollars
-		quantity: 0
-	});
-
-	var ShoppingCartModel = Backbone.Model.extend({
-		items: [] // collection of ProductModels
-	});
+	enforceDefine: true
+});
 
 
-	// now for some views
-	var UberView = Backbone.View.extend({}); // TODO setup some shit so it shows a little of the shopping cart model + image tiles
+define(
+	[
+		"jquery", 
+		"contrib/underscore", 
+		"contrib/backbone",
+		"contrib/bootstrap"
+	], 
 
-	var ShoppingCartView = Backbone.View.extend({}); // TODO editable view for shopping cart.
+	function($, _, Backbone) {
+		// TODO move this to something better
+		var productsJson = [
+			{
+				name: "Apple Butter",
+				sizes: [4]
+			},
+			{
+				name: "Brandy-soaked cherries",
+				sizes: [8] 
+			},
+			{
+				name: "Meyer Lemon and Rose Petal Jam",
+				sizes: [4, 12.5]
+			},
+			{
+				name: "Apricot Mustard",
+				sizes: [4] 
+			},
+			{
+				name: "Grapefruit Mint Syrup",
+				sizes: [8, 12.5] 
+			},
+			{
+				name: "Navel Orange Jam",
+				sizes: [12.5] 
+			},
+			{
+				name: "Grand Marnier-spiced cranberries",
+				sizes: [12.5]
+			}
+		];
 
-	var ProductOverlayView = Backbone.View.extend({}); // TODO show details of the product, and allow to add to cart.
+		var ProductModel = Backbone.Model.extend({
+			defaults: {
+				name: '',
+				sizes: []
+			}
+		});
 
-	
-}());
+		var ProductCollection = Backbone.Collection.extend({
+			model: ProductModel
+		});
+
+		var allProducts = new ProductCollection();
+
+		var ProductListView = Backbone.View.extend({
+			tagName: 'ul',
+			className: 'product-list-view',
+			initialize: function() {
+				$('body').append(this.$el);
+				this.listenTo(this.collection, "initialized", this.render);
+			},
+			render: function() {
+				this.$el.empty();
+
+				this.collection.forEach(function(product) {
+					this.$el.append($('<li><strong>' + product.get('name') + '</strong>. ' + product.get('sizes').join(',') + '</li>'));
+				}.bind(this));
+
+				return this;
+			}
+		});
+
+		var mainView = new ProductListView({
+			collection: allProducts
+		});
+
+		productsJson.forEach(function(productData) {
+			var product = new ProductModel({
+				name: productData.name,
+				sizes: productData.sizes
+			});
+
+			allProducts.add(product);
+		});
+		allProducts.trigger('initialized');
+
+
+
+	}
+);
